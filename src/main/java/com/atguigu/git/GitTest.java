13e623dda938f4a1cf060875049634a97a1c6bbb
dengjun <2439764608@qq.com>
2022-07-18 11:07:07 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author djstart
+ * @creat 2022-07-18-11:05
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+    }
+}
